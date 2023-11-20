a85bd41c692e6b6d377ba07781c720574efd115a
trineke888~ <eka888@bk.ru>
2023-11-20 20:30:56 +0300

    added

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿// void PrintMatrix(int[,] matr) // [кол-во строк, кол-во столбцов]
+// {
+//     for (int i = 0; i < matr.GetLength(0); i++) // по стр, m = matrix.GetLength(0)
+//     {
+//         for (int j = 0; j < matr.GetLength(1); j++)// по стлб, n = matrix.GetLength(1)
+//         {
+//             Console.Write($"{matr[i, j]}\t"); 
+//         }
+//         Console.WriteLine();
+//     }
+// }
